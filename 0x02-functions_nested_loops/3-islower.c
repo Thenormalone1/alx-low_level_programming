c94b3d007fb9cea00ba90d4c2ef6443a54211fd3
Youssef Ashraf <youssef42004@gmail.com>
2023-07-18 15:52:30 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is lowercase
+ * Return:  1 (lowercase) 0 (other)
+ */
+int _islower(int c)
+{
+if ('c' >= 97 && 'c' <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int  _putchar();
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void int _islower(int c);
