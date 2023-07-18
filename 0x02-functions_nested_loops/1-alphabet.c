b508befdcb389f23e878db7bbb4ce1679a3e8d0c
Youssef Ashraf <youssef42004@gmail.com>
2023-07-18 15:11:31 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void) // prints lowercase alphabet
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
 {
 _putchar(x);
 }
+ _putchar('\n');
 }
