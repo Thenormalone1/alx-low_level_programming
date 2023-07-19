62dcc80223d6c333b6f0c681379c8ad0dd6ba801
Youssef Ashraf <youssef42004@gmail.com>
2023-07-18 17:37:50 -0700

    check sign

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit(int) - print last digit
+ * @n: integer to get last digit
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+int x;
+x = n % 10;
+return (x);
+}
