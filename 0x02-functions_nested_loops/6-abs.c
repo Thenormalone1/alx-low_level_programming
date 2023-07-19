152dfc31fbbd4cb665e26ef8fe5e48fa437b2800
Youssef Ashraf <youssef42004@gmail.com>
2023-07-18 17:29:36 -0700

    check sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute
+ * @n: integer to be absolute
+ * Return: absolute ineger 
+ */
+
+int _abs(int n)
+{
+int x;
+if (n >= 0)
+{
+x = n;
+}
+else if (n < 0)
+{
+x = n * -1;
+}
+return (x);
+}
