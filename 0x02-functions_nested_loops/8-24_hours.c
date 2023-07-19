494619e0882a88c8f2196fa93a4a70b1fed052df
Youssef Ashraf <youssef42004@gmail.com>
2023-07-18 18:52:31 -0700

    check sign

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+int a;
+int b;
+for (a = 0; a <= 23; a++)
+{
+while (a <= 9)
+{
+_putchar('0');
+}
+_putchar(a);
+_putchar(':');
+for (b = 0; b <= 59; b++)
+{
+while (b <= 9)
+{
+_putchar('0');
+}
+_putchar(b);
+}
+_putchar('\n');
+}
+}
