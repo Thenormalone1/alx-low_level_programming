8c28205025a3d7eb2a3b67e88d4086923aa2fd7c
Youssef Ashraf <youssef42004@gmail.com>
2023-07-20 03:30:45 -0700

    0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_sign - checks sign
+ * @n: integer to be checked
+ * Return: 1 (Positive), 0 (Zero), -1 (Negative)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
