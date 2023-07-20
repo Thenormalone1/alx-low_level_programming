62590160ad0721a8c27eb6579306d78cdb8f3b47
Youssef Ashraf <youssef42004@gmail.com>
2023-07-20 03:33:16 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_sign - checks sign
- * @n: integer to be checked
+ * mul - checks sign
+ * @b: integar
+ * @a: integer to be checked
  * Return: 1 (Positive), 0 (Zero), -1 (Negative)
  */
 int mul(int a, int b)
