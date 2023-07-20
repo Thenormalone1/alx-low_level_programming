111f2c192313c04dcd130a6b5d6e3f83337d67e2
Youssef Ashraf <youssef42004@gmail.com>
2023-07-20 04:10:30 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ * Return: 1 (Positive), 0 (Zero), -1 (Negative)
+ */
+void print_numbers(void)
+{
+int x;
+for (x = '0'; x <= '9';x++)
+{
+_putchar(x);
+}
+}
