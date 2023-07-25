686901d7faa6c0f80aac6e5eebacc836b21a2a67
Youssef Ashraf <youssef42004@gmail.com>
2023-07-25 13:19:06 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string
+ * @dest: pointer
+ * @src: string to be copied
+ * Return: the pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len, i;
+len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
