aa542800efabf90c38769944c18766326c5bc2c0
Youssef Ashraf <youssef42004@gmail.com>
2023-07-25 07:47:24 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,12 +2,11 @@
 
 /**
  * puts2 - prints one char out of 2 of a string
- * followed by a new line
- * @str: string to print the chars from
+ * @str: string to print
  */
 void puts2(char *str)
 {
-int len, i;
+int len, x;
 len = 0;
 while (str[len] != '\0')
 {
