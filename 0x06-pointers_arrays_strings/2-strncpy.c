1f98c5aed52c0d7abcb597d9d7918f569913bf49
Youssef Ashraf <youssef42004@gmail.com>
2023-07-27 14:31:24 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates 2 strings
+ * @dest: string 1
+ * @src: string 2
+ * @n: integar for num of bytes
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int l1, l2;
+l1 = 0;
+l2 = 0;
+while (*(src + l1) != '\0' && (l1 < 97) && (l1 < n))
+{
+*(dest + l1) = *(src + l1);
+l1++;
+}
+if (l1 < n)
+{
+*(dest + l1) = '\0';
+}
+return (dest);
+}
