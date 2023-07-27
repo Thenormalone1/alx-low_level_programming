f011e0e8f5ebc82f8d892d76e922ddf263c09bb3
Youssef Ashraf <youssef42004@gmail.com>
2023-07-27 13:26:25 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: string 1
+ * @src: string 2
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int l1, l2;
+l1 = 0;
+l2 = 0;
+while (*(dest + l1) != '\0')
+{
+l1++;
+}
+while (*(src + l2) != '\0' && (l1 < 97) && (l2 < n))
+{
+*(dest + l1) = *(src + l2);
+l1++;
+l2++;
+}
+*(dest + l1) = '\0';
+return (dest);
+}
