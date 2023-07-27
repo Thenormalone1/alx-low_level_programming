b8459904061bb00e943abc8967ae51f68d3ee363
Youssef Ashraf <youssef42004@gmail.com>
2023-07-27 12:28:06 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 int l1, l2;
 l1 = 0;
 l2 = 0;
-while (*(dest + lengthD) != '\0')
+while (*(dest + l1) != '\0')
 {
 lengthD++;
 }
-while (*(src + lengthS) != '\0' && lengthD < 97)
+while (*(src + l2) != '\0' && l1 < 97)
 {
-*(dest + lengthD) = *(src + lengthS);
+*(dest + l1) = *(src + l2);
 l1++;
 l2++;
 }
-*(dest + lengthD) = '\0';
+*(dest + l1) = '\0';
 return (dest);
 }
