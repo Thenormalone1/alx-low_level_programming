53207c9354388b802f011ba8ac23801b192695a5
Youssef Ashraf <youssef42004@gmail.com>
2023-07-31 13:31:48 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,8 @@ unsigned int _strspn(char *s, char *accept)
 int j, i;
 j = 0;
 i = 0;
-unsigned int x = 0;
+unsigned int x;
+x = 0;
 while (s[i] != ' ' && s[i] != '\0')
 {
 while (accept[j] != '\0')
