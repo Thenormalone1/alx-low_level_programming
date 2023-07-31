ea429d3ef25a52fc1ef7bef8335def2893b829f3
Youssef Ashraf <youssef42004@gmail.com>
2023-07-31 13:34:30 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,15 @@ j = 0;
 i = 0;
 unsigned int x;
 x = 0;
-while (s[i] != ' ' && s[i] != '\0')
+while ((s[i] != ' ') && (s[i] != '\0'))
 {
 while (accept[j] != '\0')
 {
 if (s[i] == accept[j])
+{
 x++;
 j++;
+}
 }
 i++;
 j = 0;
