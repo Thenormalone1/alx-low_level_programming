4226ddbffb039ae1fe6ef966d9323f500f4efc2d
Youssef Ashraf <youssef42004@gmail.com>
2023-07-31 13:29:41 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - find char
+ * @s: char
+ * @accept: char
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int j = 0, i = 0;
+unsigned int x = 0;
+while (s[i] != ' ' && s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+x++;
+j++;
+}
+i++;
+j = 0;
+}
+return (x);
+}
