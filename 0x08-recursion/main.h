7e0bfa5fa567e158d1cc1fb929d49c5fbea1c054
Youssef Ashraf <youssef42004@gmail.com>
2023-08-03 13:08:41 -0700

    6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
 void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
+77;30604;0cvoid _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -9,3 +9,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 int _sqrt(int a, int b);
+int _prime(int n, int i);
