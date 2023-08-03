2b8254a67ee7bae4396635fbd3de8c6d0c09e5a0
Youssef Ashraf <youssef42004@gmail.com>
2023-08-03 11:33:24 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints char
+ * @s: integer to be checked
+ */
+void _puts_recursion(char *s)
+{
+int x;
+x = 0;
+while (s[x] != '\0')
+{
+_putchar(s[x];
+x++;
+}
+_putchar('\n');
+}
