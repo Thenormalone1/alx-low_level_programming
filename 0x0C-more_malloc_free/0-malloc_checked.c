ba2370fd379f565f8264ed7b15a1327adc62c08d
Youssef Ashraf <youssef42004@gmail.com>
2023-08-10 15:14:21 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - checks sign
+ * @b: int
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (b > 0)
+	{
+	return (98);
+	}
+	return (malloc(b);
+}
