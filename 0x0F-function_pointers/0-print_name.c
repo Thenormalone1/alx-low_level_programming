93844ce5e66dbe5551031a869b3ec455950590eb
Youssef Ashraf <youssef42004@gmail.com>
2023-08-16 16:41:26 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include "function_pointers.h"
 /**
  * print_name - prints name
  * @name string
  * @f: pointer to a function
  */
-void print_name1(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);
 }
