96b0799a0324412e232c7a052496f23e0a2abaab
Youssef Ashraf <youssef42004@gmail.com>
2023-08-16 17:00:13 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
-	if (array == NULL || size == NULL || action == NULL)
+	if (array == NULL || size == 0 || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
