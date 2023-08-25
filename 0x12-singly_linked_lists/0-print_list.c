8de9f726006773a9a8efb3a49a976908b98f90f4
Youssef Ashraf <youssef42004@gmail.com>
2023-08-24 19:21:32 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * print_listint - prints list
+  * @h: list pointer
+  * Return: number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t x;
+
+	for (x = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
